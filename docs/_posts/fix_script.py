81e7ea5ed03e1bbbae345d05fafe32380615f345
ThriftyOldStudent <asvoria@live.com>
2022-04-27 20:25:45 +0800

    Mass Fix

---
+++ docs/_posts/fix_script.py
@@ -0,0 +1,15 @@
+
+import os
+
+newFileName = []
+stringFind = 'link: https://www.royalroad.com/fiction/'
+
+for filename in os.listdir(os.getcwd()):
+   if filename.endswith(".md"):
+      with open(os.path.join(os.getcwd(), filename), 'r') as rawfile: 
+         for line in rawfile:
+            if stringFind in line:
+               x = line.split("/")
+               newFileName = "2022-03-20-F" + x[4] + ".md"
+               break
+      os.rename(filename, newFileName)
---
+++ readme.md
@@ -0,0 +1,5 @@
+Planning on the updates
+1. Have a script to fix the file name not matching the RRID.
+2. Have a single file that include all RRID
+3. Use the RSS syndicate to track latest update.
+4. Automatically assign HIATUS status on works 2 months without update.
