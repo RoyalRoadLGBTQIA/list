f30272cdd918fc1a42a24fafef18df2eba08d6c6
ThriftyOldStudent <asvoria@live.com>
2022-04-28 20:12:54 +0800

    up

--- docs/_posts/fix_script.py
+++ docs/_posts/fix_script.py
@@ -8,7 +8,9 @@ newFileName = []
 stringSyndi = 'https://www.royalroad.com/syndication/'
 
 for filename in os.listdir(os.getcwd()):
-   if filename.endswith(".md"):
+   if filename.startswith("2022-04-28-F"):
+      print ("Skip...")
+   else:
       basename = os.path.basename(filename)
       rrid_arr = basename.split("-")
       rrid = rrid_arr[3].replace(".md", "")
@@ -20,13 +22,27 @@ for filename in os.listdir(os.getcwd()):
          date_str = date_time_str[1] + " " + date_time_str[2] + " " + date_time_str[3]
          date_time_obj = datetime.strptime(date_str, '%d %b %Y')
          newFileName = date_time_obj.strftime("%Y-%m-%d") + "-" + rrid + ".md"
+         delta = datetime.today() - date_time_obj
+
+         if delta.days > 40:
+            print(delta.days)
+            stringFind = 'ONGOING'
+            with open(os.path.join(os.getcwd(), filename), 'r+') as rawfile: 
+               mark_i = 0
+               for line in rawfile:
+                  if stringFind in line:
+                     line = line.replace(stringFind,'HIATUS')
+                     file_object = open('RRID.txt', 'a')
+                     file_object.write(line)
+                     file_object.close()
+                     break
+
          i = i + 1
          if i > 30:
             time.sleep(1)
             i = 0
-
          if newFileName in basename:
-            print ("match: " + i)
+            print ("m: " + str(i))
          else:
             os.rename(filename, newFileName)
 
