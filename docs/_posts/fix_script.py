e9c60cdbbd514b0ad0651c355e8d5768e89ee1e7
ThriftyOldStudent <asvoria@live.com>
2022-05-21 19:25:44 +0800

    update

--- docs/_posts/fix_script.py
+++ docs/_posts/fix_script.py
@@ -8,7 +8,7 @@ newFileName = []
 stringSyndi = 'https://www.royalroad.com/syndication/'
 
 for filename in os.listdir(os.getcwd()):
-   if filename.startswith("2022-05-20-F"):
+   if filename.startswith("2022-05-21-F"):
       print ("Skip...3")
    elif filename.startswith("2021"):
       print ("Skip...2021 done")
