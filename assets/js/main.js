05fdfaf401378a276d16cbc7e025791143c8c59c
ThriftyOldStudent <asvoria@live.com>
2023-02-19 03:13:10 +0800

    TAG

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ var $filterCheckboxes = $('input[type="checkbox"]');
 var filterFunc = function() {
   
   var selectedFilters = {};
-
   $filterCheckboxes.filter(':checked').each(function() {
 
     if (!selectedFilters.hasOwnProperty(this.name)) {
@@ -12,38 +11,22 @@ var filterFunc = function() {
     selectedFilters[this.name].push(this.value);
   });
 
-  // create a collection containing all of the filterable elements
   var $filteredResults = $('.entry');
-  console.log("Here...");
-  // loop over the selected filter name -> (array) values pairs
   $.each(selectedFilters, function(name, filterValues) {
-    // filter each .flower element
     $filteredResults = $filteredResults.filter(function() {
-      console.log("Entered...");
+
       var matched = false,
         currentFilterValues = $(this).data('category').split(' ');
-
-      console.log(currentFilterValues);
-      // loop over each category value in the current .flower's data-category
       $.each(currentFilterValues, function(_, currentFilterValue) {
-
-        // if the current category exists in the selected filters array
-        // set matched to true, and stop looping. as we're ORing in each
-        // set of filters, we only need to match once
-
         if ($.inArray(currentFilterValue, filterValues) != -1) {
           matched = true;
           return false;
         }
       });
-
-      // if matched is true the current .flower element is returned
       return matched;
 
     });
   });
-  console.log("Out...");
-
   $('.entry').hide().filter($filteredResults).show();
 }
 
